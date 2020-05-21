784cec9cd41f03551490a0dc871ced5da7de6099
Nate West <nwestr@gmail.com>
2020-05-21 09:58:27 -0400

    Up to challenge 3

--- src/Set1.hs
+++ src/Set1.hs
@@ -1,15 +1,14 @@
-{-# LANGUAGE GeneralizedNewtypeDeriving #-}
 {-# LANGUAGE OverloadedStrings #-}
 
 module Set1 where
 
 import Data.Word (Word8)
-import qualified Data.ByteString             as BSStrict
-import qualified Data.ByteString.Lazy.Char8  as Ch8
 import qualified Data.ByteString.Lazy        as BS
 import qualified Data.ByteString.Base16.Lazy as BS16
 import qualified Data.ByteString.Base64.Lazy as BS64
-import Data.Bits ((.|.), xor)
+import Data.ByteString.Internal (c2w, w2c)
+import Data.Bits (xor)
+import Data.List (foldl', sortOn)
 
 newtype ErrorString a = ErrorString {runError :: a} deriving (Show, Eq)
 newtype HexString a = HexString {runHex :: a} deriving (Show, Eq)
@@ -21,3 +20,66 @@ hexToBase64 a = Base64String . BS64.encode <$> decodeHex a
 decodeHex :: HexString BS.ByteString -> Either (ErrorString BS.ByteString) BS.ByteString
 decodeHex a | (decoded, "") <- BS16.decode . runHex $ a = Right decoded
             | otherwise = Left . ErrorString $ BS.concat ["invalid hex: ", runHex a]
+
+rawString :: BS.ByteString -> String
+rawString = map w2c . BS.unpack
+
+xorBytes :: BS.ByteString -> BS.ByteString -> Either (ErrorString BS.ByteString) BS.ByteString
+xorBytes a b | BS.length a == BS.length b = Right . BS.pack $ BS.zipWith xor a b
+             | otherwise = Left . ErrorString $ "Mismatched lengths for XOR"
+
+bytesToHex :: BS.ByteString -> HexString BS.ByteString
+bytesToHex = HexString . BS16.encode
+
+
+hexToRaw :: HexString BS.ByteString -> String
+hexToRaw hs = let bs = decodeHex hs
+               in case bs of
+                    Right a -> rawString a
+                    Left  b -> rawString . runError $ b
+
+hexToBytes :: HexString BS.ByteString -> BS.ByteString
+hexToBytes hs = let bs = decodeHex hs
+                 in case bs of
+                    Right a -> a
+                    Left  b -> runError b
+
+singleCharXOR :: Word8 -> BS.ByteString -> BS.ByteString
+singleCharXOR c xs = BS.pack . BS.zipWith xor cs $ xs
+                      where
+                        len = fromIntegral . BS.length $ xs
+                        cs = BS.pack . replicate len $ c
+
+singleChars :: [Word8]
+singleChars = map c2w ['A'..'z'] -- "ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\]^_`abcdefghijklmnopqrstuvwxyz"
+
+charEnglishScore :: Word8 -> Int
+charEnglishScore c | f 'e' = 200
+                   | f 't' = 180
+                   | f 'a' = 160
+                   | f 'o' = 140
+                   | f 'l' = 120
+                   | f 'i' = 100
+                   | f 'n' = 80
+                   | otherwise = -2
+                  where
+                    f x = c == c2w x
+
+
+englishScore :: BS.ByteString -> Int
+englishScore = foldl' (+) 0 . map charEnglishScore . BS.unpack
+
+input3 :: BS.ByteString
+input3 = hexToBytes . HexString $ "1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736"
+
+num3 :: BS.ByteString
+num3 = let (_, _, x) = bestSingleCharXOR input3
+        in x
+
+bestSingleCharXOR :: BS.ByteString -> (Word8, Int, BS.ByteString)
+bestSingleCharXOR xs = let scores = map f singleChars
+                        in last . sortOn (\(_, a, _) -> a) $ scores
+                       where f c = let xord = singleCharXOR c input3
+                                       score = englishScore xord
+                                    in (c, score, xord)
+
--- test/Spec.hs
+++ test/Spec.hs
@@ -3,8 +3,9 @@
 import qualified Test.Tasty
 import Test.Tasty.Hspec
 import Set1
-import Data.ByteString.Lazy
 import Control.Monad (join)
+import Data.ByteString.Internal (c2w)
+import qualified Data.ByteString.Lazy as BS
 
 main :: IO ()
 main = do
@@ -19,3 +20,26 @@ spec = parallel $ do
         let expected = Right . Base64String $ "SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t"
             result = hexToBase64 $ HexString "49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d"
         result `shouldBe` expected
+
+    describe "Challenge 2" $ do
+      it "XORs two strings of data" $ do
+        let expected  = Right . HexString $ "746865206b696420646f6e277420706c6179"
+            a = decodeHex . HexString $ "1c0111001f010100061a024b53535009181c"
+            b = decodeHex . HexString $ "686974207468652062756c6c277320657965"
+            result = bytesToHex <$> join (xorBytes <$> a <*> b)
+        result `shouldBe` expected
+
+    describe "Challenge 3" $ do
+
+      it "converts a byte string back to a raw string" $ do
+        let hexString = HexString $ "6669747465722c206861707065722c206d6f72652070726f64756374697665"
+            result = hexToRaw hexString
+            expected = "fitter, happer, more productive"
+        result `shouldBe` expected
+
+      it "computes a single char based XOR" $ do
+        let input = hexToBytes . HexString $ "1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736"
+            (_, _, result) = bestSingleCharXOR input
+            expected = "Cooking MC's like a pound of bacon"
+        result `shouldBe` expected
+
