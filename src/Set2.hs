7e5371be4179104917d8ff8ea6e2b84b701a1f9b
Nate West <nwestr@gmail.com>
2020-06-05 11:43:41 -0400

    Challenge 11

--- src/Set2.hs
+++ src/Set2.hs
@@ -4,8 +4,10 @@ module Set2 where
 
 import qualified Data.ByteString.Lazy as BS
 import Data.ByteString.Internal (c2w)
+import qualified Data.Set as S
 import OpenSSL.EVP.Base64
 
+import Helpers
 import NWOpenSSL
 
 num9 :: BS.ByteString
@@ -17,4 +19,13 @@ num10 = do
   let key = BS.pack . map c2w $ "YELLOW SUBMARINE"
       iv  = BS.replicate 16 0
   aes128cbcDecrypt iv key bytes
+
+num11 :: IO String
+num11 = do
+  boxBytes <- encryptionOracle . BS.take (10 * 16) . BS.cycle $ "YELLOW SUBMARINE"
+  let variations = map (\i -> takeChunks 16 . BS.drop i $ boxBytes) [3..12]
+      sizes = map (\bs -> (length bs, S.size . S.fromList $ bs)) variations
+      smaller = filter (\(a, b) -> b < a) sizes
+  if null smaller then return "It's probably CBC"
+                  else return "It's probably ECB"
 
