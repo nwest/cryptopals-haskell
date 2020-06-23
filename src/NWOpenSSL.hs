7bf51ca4c606fcff8fd1d6bb33d89c248a485e44
Nate West <nwestr@gmail.com>
2020-06-23 14:25:16 -0400

    Finish #12

--- src/NWOpenSSL.hs
+++ src/NWOpenSSL.hs
@@ -1,20 +1,25 @@
-{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE OverloadedStrings, FlexibleContexts #-}
 
 module NWOpenSSL where
 
 import           Helpers
 import           Codec.Crypto.SimpleAES (Key, Direction(..), Mode(..), crypt, randomKey)
 import qualified Data.ByteString.Lazy as Lazy (fromStrict, toStrict)
-import qualified Data.ByteString as ByteString (append, length, concat, pack)
+import qualified Data.ByteString as ByteString (append, length, concat, pack, take)
 import           Data.ByteString.Char8 (ByteString)
-import qualified Data.ByteString.Char8 as Char8 (replicate, pack)
+import qualified Data.ByteString.Char8 as Char8 (replicate, pack, snoc)
 import           Data.Char (chr)
-import           Data.List (foldl')
+import           Data.List (foldl', findIndex)
 import           Data.Vector (toList)
+import qualified Data.HashMap.Strict as HashMap (empty, insert, lookup)
+import           Data.Maybe (fromJust)
+
+
 import           System.Random.MWC
 import           Debug.Trace
 
 type IV = ByteString
+type Encryptor = ByteString -> ByteString
 
 pkcs7pad :: Int -> ByteString -> ByteString
 pkcs7pad size bytes | byte > 0 = ByteString.append bytes paddingString
@@ -25,6 +30,9 @@ pkcs7pad size bytes | byte > 0 = ByteString.append bytes paddingString
 
 zeroIV :: ByteString
 zeroIV = Char8.replicate 16 '\0'
+
+randomAESKey :: IO ByteString
+randomAESKey = randomBytes 16
 
 encryptECB :: Key -> ByteString -> ByteString
 encryptECB key = Lazy.toStrict . crypt ECB key zeroIV Encrypt . Lazy.fromStrict
@@ -68,7 +76,41 @@ encryptionOracle bs = do
   where
     ecb xs = flip encryptECB xs <$> randomKey
     cbc xs = flip (uncurry encryptCBC) xs <$> ((,) <$> randomKey <*> randomKey)
-   
+
 ecbEncryptionOracle :: ByteString -> ByteString -> IO ByteString
 ecbEncryptionOracle key bs = encryptECB key <$> randomPad bs
+
+ecbKeySize :: Encryptor -> Int
+ecbKeySize enc = head . filter (isECB enc) $ [1..64]
+
+isECB :: Encryptor -> Int -> Bool
+isECB enc size = let (a:b:_) = takeChunks size . enc . Char8.replicate (size * 2) $ '\0'
+                  in a == b
+
+ecbPlaintextLength :: Encryptor -> Int
+ecbPlaintextLength enc = let steps = map (ByteString.length . enc . flip Char8.replicate '\0') [0..]
+                             Just paddingSize = findIndex (/= 0) . zipWith (-) (tail steps) $ steps
+                          in head steps - paddingSize
+
+breakECB :: Encryptor -> Int -> ByteString
+breakECB enc blockSize = foldl' (\bs i -> 
+  Char8.snoc bs (breakECBByte blockSize enc i bs)) 
+  "" 
+  [1..ecbPlaintextLength enc]
+
+breakECBByte :: Int -> Encryptor -> Int -> ByteString -> Char
+breakECBByte size encryptor index bs = 
+    let prefixLength = size - (index - 1) `mod` size - 1
+        zeros = Char8.replicate prefixLength '\0'
+        targetSize = (1 + (index - 1) `quot` size) * size
+        target = ByteString.take targetSize $ encryptor zeros
+        prefix = ByteString.append zeros bs
+        dict = foldr
+                  (\x m ->
+                      let plaintext = Char8.snoc prefix (chr x)
+                          ciphertext = encryptor plaintext
+                       in HashMap.insert (ByteString.take targetSize ciphertext) x m)
+                  HashMap.empty
+                  [0..255]
+    in chr . fromJust $ HashMap.lookup target dict
 
--- src/Set2.hs
+++ src/Set2.hs
@@ -3,19 +3,15 @@
 module Set2 where
 
 import qualified Data.Set as S
--- import           Data.List (elemIndex, findIndex)
-
+import           Codec.Crypto.SimpleAES (Key)
 import qualified Data.ByteString.Lazy as Lazy (cycle, take, toStrict)
-import qualified Data.ByteString as ByteString (drop, readFile)
+import qualified Data.ByteString as ByteString (drop, readFile, append)
 import           Data.ByteString.Char8 (ByteString)
 import qualified Data.ByteString.Char8 as Char8 (pack, filter)
 import qualified Data.ByteString.Base64 as Base64 (decodeLenient)
--- import           Data.Char (ord)
 
 import NWOpenSSL
 import Helpers
-
-type Encryptor = ByteString -> ByteString
 
 num9 :: ByteString
 num9 = pkcs7pad 20 "YELLOW SUBMARINE"
@@ -38,42 +34,15 @@ num11 = do
 num12Bytes :: ByteString
 num12Bytes = Base64.decodeLenient "Um9sbGluJyBpbiBteSA1LjAKV2l0aCBteSByYWctdG9wIGRvd24gc28gbXkgaGFpciBjYW4gYmxvdwpUaGUgZ2lybGllcyBvbiBzdGFuZGJ5IHdhdmluZyBqdXN0IHRvIHNheSBoaQpEaWQgeW91IHN0b3A/IE5vLCBJIGp1c3QgZHJvdmUgYnkK"
 
--- num12 :: IO ()
--- num12 = do
---   key <- randomAESKey
---   let sizeTests = map (\b -> BS.append b b) . map (BS.pack . map c2w . flip replicate 'A') $ [1..64]
---   testBytes <- map (\(i, bs)-> BS.take (i * 2) bs) . zip [1..] <$> (sequence . map (aes128ecbEncrypt key) $ sizeTests)
---   let keySize = ecbKeySize testBytes
---   print keySize
---   let rawDictionaryValues = map (BS.snoc (BS.replicate (fromIntegral . pred $ keySize) (c2w 'A'))) $ [0..255]
---   encryptedDictionaryValues <- sequence . map (aes128ecbEncrypt key) $ rawDictionaryValues
--- 
---   let dictionary = map (BS.take (fromIntegral keySize)) $ encryptedDictionaryValues
---       as = BS.pack . map c2w . replicate (pred keySize) $ 'A' 
--- 
---   bss <- map (BS.take (fromIntegral keySize)) <$> (sequence . map (aes128ecbEncrypt key) . map (BS.snoc as) $ BS.unpack num12Bytes)
--- 
---   let secret = BS.pack . map (fromIntegral . f . flip elemIndex dictionary) $ bss
---   print secret
---     where
---       f Nothing = 0
---       f (Just x) = x
--- 
--- ecbKeySize :: [BS.ByteString] -> Int
--- ecbKeySize bs = let sizes = map (\(i, xs) -> (i, setSize i xs)) . zip [1..] $ bs
---                     smaller = filter (\(_, (b, c)) -> b > c) sizes
---                  in if null smaller then 0
---                                     else fst . head $ smaller
---                   where
---                     setSize i xs = let chunks = takeChunks i xs
---                                    in (length chunks, S.size . S.fromList $ chunks)
--- 
--- inferEBCInfo :: Encryptor -> (Int, Int)
--- inferEBCInfo f =
---     let lengths = map (\i -> ByteString.length $ f (Char8.replicate i '\0')) [0..]
---         transitions = zipWith (-) (drop 1 lengths) lengths
---         chunkSize = head $ dropWhile (== 0) transitions
---         Just offset = findIndex (/= 0) transitions
---         plaintextLength = head lengths - offset
---     in (chunkSize, plaintextLength)
+num12Encryptor :: Key -> Encryptor
+num12Encryptor key bs = encryptECB key $ ByteString.append bs num12Bytes
+
+num12 :: IO ()
+num12 = do
+  key <- randomAESKey
+  let encryptor = num12Encryptor key
+      keySize = ecbKeySize encryptor
+  print keySize
+  print $ isECB encryptor keySize
+  print $ breakECB encryptor keySize
 
